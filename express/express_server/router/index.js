69d089ba6cc0e9abe4d513b26e9adb422230b07c
qilinli@aliyun.com1 <18169791243@163.com>
2020-01-30 22:16:36 +0800

    跨域实现jsonp请求

--- express/express_server/router/index.js
+++ express/express_server/router/index.js
@@ -16,5 +16,14 @@ Router.use('/goods', goodsRouter)
 // 不管什么请求类型，只要请求路径是/reg就会进入
 Router.use('/reg', regRouter)
 
+// jsonp实现跨域请求
+Router.get('/jsonp', (req, res, next) => {
+    // 从数据读取数据
+    let data = { username: 'min', password: 123 }
+    // 接收前端传入的函数名
+    let { callback } = req.query // getData
+    res.send(`${callback}(${data})`)
+})
+
 // 导出Router（暴露）
 module.exports = Router
